d3950b4d43cbe7461cf5dda5a76997973d4fe41f
jordanIsNotACoder <iraborjordan1@gmail.com>
2019-02-11 12:20:13 +0100

    .gitignore is now working

---
+++ frontend/src/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./NotFound.css";
+
+export default class NotFound extends Component {
+    render(){
+
+        return (
+            <div className="NotFound">Page Not Found</div>
+        )
+    }
+}
---
+++ frontend/src/SecuredRoute/SecuredRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import auth0Client from '../Auth';
+
+function SecuredRoute(props) {
+    const { component: Component, path, checkingSession } = props;
+    return (
+        <Route path={path} render={() => {
+            if (checkingSession) return <p style={{ 'padding': '50vh' }}>Validating session...</p>;
+            if (!auth0Client.isAuthenticated()) {
+                auth0Client.signIn();
+                return <div style={{ 'padding': 0 }}></div>;
+            }
+            return <Component />
+        }} />
+    );
+}
+
+export default SecuredRoute;
